8ccb8de05e2b6131de4156a84fd29a580e8a9fcb
Prajwal Aradhya <manuaradhya07@gmail.com>
2026-06-24 01:00:40 +0100

    fix lint issue

--- apps/server/src/api/v1/integrations/get-by-id/route.ts
+++ apps/server/src/api/v1/integrations/get-by-id/route.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import {
-  describeRoute,
-  DescribeRouteOptions,
-  resolver,
-  validator,
+	describeRoute,
+	DescribeRouteOptions,
+	resolver,
+	validator,
 } from "hono-openapi";
 import { requestRouteSchema, responseSchema } from "./dto";
 import handleRequest from "./service";
@@ -13,39 +13,39 @@ import { HonoServer } from "../../../../types";
 import { requireRoleAccess } from "../../../auth/middleware";
 
 const openapiRouteOptions: DescribeRouteOptions = {
-  description: "Get integration by id",
-  operationId: "get-integration-by-id",
-  tags: ["Integrations"],
-  responses: {
-    200: {
-      description: "Successful",
-      content: {
-        "application/json": {
-          schema: resolver(responseSchema),
-        },
-      },
-    },
-    404: {
-      description: "Integration not found",
-      content: {
-        "application/json": {
-          schema: resolver(errorSchema),
-        },
-      },
-    },
-  },
+	description: "Get integration by id",
+	operationId: "get-integration-by-id",
+	tags: ["Integrations"],
+	responses: {
+		200: {
+			description: "Successful",
+			content: {
+				"application/json": {
+					schema: resolver(responseSchema),
+				},
+			},
+		},
+		404: {
+			description: "Integration not found",
+			content: {
+				"application/json": {
+					schema: resolver(errorSchema),
+				},
+			},
+		},
+	},
 };
 
 export default function (app: HonoServer) {
-  app.get(
-    "/:id",
-    describeRoute(openapiRouteOptions),
-    requireRoleAccess("creator"),
-    validator("param", requestRouteSchema, zodErrorCallbackParser),
-    async (c) => {
-      const id = c.req.param("id");
-      const result = await handleRequest(id);
-      return c.json(result);
-    }
-  );
+	app.get(
+		"/:id",
+		describeRoute(openapiRouteOptions),
+		requireRoleAccess("creator"),
+		validator("param", requestRouteSchema, zodErrorCallbackParser),
+		async (c) => {
+			const id = c.req.param("id");
+			const result = await handleRequest(id!);
+			return c.json(result);
+		},
+	);
 }
